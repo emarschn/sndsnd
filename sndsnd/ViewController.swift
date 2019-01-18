18919786c19095b9095c7d756cfa53a4e5ceaa4c
Eric Marschner <emarschn@gmail.com>
2019-01-18 14:36:32 -0700

    add waveform

--- sndsnd/ViewController.swift
+++ sndsnd/ViewController.swift
@@ -10,18 +10,22 @@ import UIKit
 import AVKit
 import AVFoundation
 import AudioToolbox
+import Accelerate
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var indicatorView: UIActivityIndicatorView!
-    @IBOutlet weak var rec: UIButton!
-    @IBOutlet weak var mic: UILabel!
+    @IBOutlet weak var wave0: WaveformView!
+    @IBOutlet weak var wave1: WaveformView!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var inputLabel: UILabel!
 
     var audioEngine : AVAudioEngine!
     var mixer : AVAudioMixerNode!
     var recording = false
 
-    let routePickerView = AVRoutePickerView(frame: .init(x: 0, y: 0, width: 40, height: 40))
+    private var averagePowerForChannel0: Float = 0
+    private var averagePowerForChannel1: Float = 0
+    let LEVEL_LOWPASS_TRIG:Float32 = 0.30
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,7 +48,6 @@ class ViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
 
-        routePickerView.center = self.view.center;
     }
 
     deinit {
@@ -62,9 +65,14 @@ class ViewController: UIViewController {
     }
 
     func setupView() {
-        self.indicator(show: false)
-        self.mic.text = ""
-        view.addSubview(routePickerView)
+        self.inputLabel.text = ""
+
+        let routePickerView = AVRoutePickerView(frame: .init(x: 0, y: 0, width: 40, height: 40))
+        let but = UIBarButtonItem.init(customView: routePickerView)
+        self.navigationItem.rightBarButtonItem = but
+
+        let displayLink = CADisplayLink(target: self, selector: #selector(updateMeters))
+        displayLink.add(to: RunLoop.current, forMode: RunLoop.Mode.common)
     }
 
     func setupNotifications() {
@@ -85,10 +93,18 @@ class ViewController: UIViewController {
 
         let sampleRate = AVAudioSession.sharedInstance().sampleRate
 
-        let inFormat = self.audioEngine.inputNode.inputFormat(forBus: 0)
+        let inputNode = self.audioEngine.inputNode
+        let inFormat = inputNode.inputFormat(forBus: 0)
 
-        self.audioEngine.connect(self.audioEngine.inputNode, to: self.mixer, format: inFormat)
+        self.audioEngine.connect(inputNode, to: self.mixer, format: inFormat)
         self.audioEngine.connect(self.mixer, to: self.audioEngine.mainMixerNode, format: inFormat)
+
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: inFormat) {[weak self] (buffer:AVAudioPCMBuffer, when:AVAudioTime) in
+            guard let strongSelf = self else {
+                return
+            }
+            strongSelf.audioMetering(buffer: buffer)
+        }
 
         self.mixer.installTap(onBus: 0, bufferSize: AVAudioFrameCount(sampleRate * 0.4), format: inFormat, block: { (buffer: AVAudioPCMBuffer!, time: AVAudioTime!) -> Void in
 
@@ -112,6 +128,11 @@ class ViewController: UIViewController {
         UIApplication.shared.isIdleTimerDisabled = false
     }
 
+    @objc func updateMeters() {
+        wave0.updateWithLevel(CGFloat(pow(10, self.averagePowerForChannel0 / 20)))
+        wave1.updateWithLevel(CGFloat(pow(10, self.averagePowerForChannel1 / 20)))
+    }
+
     func completion() {
         if self.recording {
             DispatchQueue.main.async {
@@ -120,27 +141,13 @@ class ViewController: UIViewController {
         }
     }
 
-    func indicator(show: Bool) {
-        DispatchQueue.main.async {
-            if show {
-                self.indicatorView.startAnimating()
-                self.indicatorView.isHidden = false
-            } else {
-                self.indicatorView.stopAnimating()
-                self.indicatorView.isHidden = true
-            }
-        }
-    }
-
     @IBAction func rec(_ sender: Any) {
         if self.recording {
-            self.indicator(show: false)
             self.stopPassThru()
-            self.rec.setTitle("START", for: .normal)
+            self.startButton.setTitle("START", for: .normal)
         } else {
-            self.indicator(show: true)
             self.startPassThru()
-            self.rec.setTitle("STOP", for: .normal)
+            self.startButton.setTitle("STOP", for: .normal)
         }
     }
 
@@ -161,19 +168,47 @@ class ViewController: UIViewController {
     }
 
     func checkRoutes() {
-        mic.isHidden = true
+        inputLabel.isHidden = true
         let route = AVAudioSession.sharedInstance().currentRoute
         for port in route.inputs {
             if port.portType == AVAudioSession.Port.lineIn {
-                mic.text = "✅"
-                mic.isHidden = false
+                inputLabel.text = "✅"
+                inputLabel.isHidden = false
             } else if port.portType == AVAudioSession.Port.headsetMic {
-                mic.text = "🎧"
-                mic.isHidden = false
+                inputLabel.text = "🎧"
+                inputLabel.isHidden = false
             } else if port.portType == AVAudioSession.Port.builtInMic {
-                mic.text = "🎙"
-                mic.isHidden = false
+                inputLabel.text = "📱"
+                inputLabel.isHidden = false
             }
+        }
+    }
+
+    // https://stackoverflow.com/questions/30641439/level-metering-with-avaudioengine/30651552
+    private func audioMetering(buffer:AVAudioPCMBuffer) {
+        buffer.frameLength = 1024
+        let inNumberFrames:UInt = UInt(buffer.frameLength)
+        if buffer.format.channelCount > 0 {
+            let samples = (buffer.floatChannelData![0])
+            var avgValue:Float32 = 0
+            vDSP_meamgv(samples,1 , &avgValue, inNumberFrames)
+            var v:Float = -100
+            if avgValue != 0 {
+                v = 20.0 * log10f(avgValue)
+            }
+            self.averagePowerForChannel0 = (self.LEVEL_LOWPASS_TRIG*v) + ((1-self.LEVEL_LOWPASS_TRIG)*self.averagePowerForChannel0)
+            self.averagePowerForChannel1 = self.averagePowerForChannel0
+        }
+
+        if buffer.format.channelCount > 1 {
+            let samples = buffer.floatChannelData![1]
+            var avgValue:Float32 = 0
+            vDSP_meamgv(samples, 1, &avgValue, inNumberFrames)
+            var v:Float = -100
+            if avgValue != 0 {
+                v = 20.0 * log10f(avgValue)
+            }
+            self.averagePowerForChannel1 = (self.LEVEL_LOWPASS_TRIG*v) + ((1-self.LEVEL_LOWPASS_TRIG)*self.averagePowerForChannel1)
         }
     }
 
